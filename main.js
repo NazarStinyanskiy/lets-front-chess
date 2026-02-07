b5488797b2647508b0b856744fa4e2a9e7e2dcc1
nstinianskyi <nstinianskiy@magnite.com>
2026-02-07 23:31:11 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function createField() {
+    let board = document.getElementById("board");
+    let cells = [];
+    for (var i = 1; i <= 8; i++) {
+        let letter = String.fromCharCode('a'.charCodeAt(0) + i - 1)
+        for (var j = 1; j <= 8; j++) {
+            let colorClass = "white";
+            if (i % 2 != 0 && j % 2 == 0 || i % 2 == 0 && j % 2 != 0) {
+                colorClass = "grey";
+            }
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.classList.add(colorClass);
+
+            let cellPositionP = document.createElement("p");
+            cellPositionP.textContent = letter + j
+            
+            cell.appendChild(cellPositionP)
+            board.appendChild(cell);
+        }
+    }
+}
+
+createField();
